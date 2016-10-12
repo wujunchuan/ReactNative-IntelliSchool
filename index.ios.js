5ecb3b723032d6d29c5da43f270459aeca6d1517
JohnTrump <wujunchuan1994@hotmail.com>
2016-10-12 19:33:53 +0800

    add demo/states

--- index.ios.js
+++ index.ios.js
@@ -4,50 +4,60 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+	AppRegistry,
+	StyleSheet,
+	Text,
+	View
 } from 'react-native';
 
+class Bink extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {showText: true};
+		setInterval(()=> {
+			this.setState({showText: !this.state.showText})
+		}, 1000);
+	}
+
+	render() {
+		let display = this.state.showText ? this.props.text : ' ';//如果showText为假 就不现实字符串
+		return(
+			<Text>{display}</Text>
+		)
+	}
+}
+
 export default class ReactNative_intelliSchool extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+	render() {
+		return (
+			<View>
+				<Bink text="Hello world"/>
+				<Bink text="hahahaha"/>
+				<Bink text="Hello world"/>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#F5FCFF',
+	},
+	welcome: {
+		fontSize: 20,
+		textAlign: 'center',
+		margin: 10,
+	},
+	instructions: {
+		textAlign: 'center',
+		color: '#333333',
+		marginBottom: 5,
+	},
 });
 
 AppRegistry.registerComponent('ReactNative_intelliSchool', () => ReactNative_intelliSchool);
