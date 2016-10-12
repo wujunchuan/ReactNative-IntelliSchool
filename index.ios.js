d7558507d8cc75a07428d0797f1ed5e5570f1760
JohnTrump <wujunchuan1994@hotmail.com>
2016-10-12 20:08:17 +0800

    学会了flex

--- index.ios.js
+++ index.ios.js
@@ -5,6 +5,7 @@
  */
 
 import React, {Component} from 'react';
+//noinspection JSUnresolvedVariable
 import {
 	AppRegistry,
 	StyleSheet,
@@ -16,10 +17,10 @@ import {
 export default class ReactNative_intelliSchool extends Component {
 	render() {
 		return (
-			<View>
-				<View style={{width:50,height:50,backgroundColor:'powderblue'}}></View>
-				<View style={{width:100,height:100,backgroundColor:'skyblue'}}></View>
-				<View style={{width:150,height:150,backgroundColor:'steelblue'}}></View>
+			<View style={{height:300}}>
+				<View style={{flex:1,backgroundColor:'powderblue'}}></View>
+				<View style={{flex:2,backgroundColor:'skyblue'}}></View>
+				<View style={{flex:3,backgroundColor:'steelblue'}}></View>
 			</View>
 		);
 	}
