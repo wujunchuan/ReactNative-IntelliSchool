8ae6375f51ecaa27419df02e0ea17655a6fe7b65
JohnTrump <wujunchuan1994@hotmail.com>
2016-10-13 22:09:16 +0800

    Simple Navigator get~

--- index.ios.js
+++ index.ios.js
@@ -11,44 +11,52 @@ import {
 	StyleSheet,
 	Text,
 	View,
-	ListView,
+	TouchableHighlight,
+	Navigator,
 } from 'react-native';
 
-export default class ReactNative_intelliSchool extends Component {
-	constructor(props){
-		super(props);
-		const ds = new ListView.DataSource({
-			rowHasChanged: (r1, r2) => r1 !== r2
-		});
-		this.state = {
-			dataSource: ds.cloneWithRows([
-				'wujunchuan', 'Joel', 'Java', 'Javascript', 'Jackson', 'Judile', 'Devie',
-				'haha','hehe','fuck','enen'
-			])
-		};
-	}
+export default class SimpleNavigationApp extends Component {
 	render() {
 		return (
-			<View style={{paddingTop: 22,flex:1}}>
-				<ListView
-					dataSource={this.state.dataSource}
-					renderRow={(rowData) => <Text style={{fontSize:50}}>{rowData}</Text>}
-					showVerticalScrollIndicator={false}
-				/>
-			</View>
-		);
+			<Navigator
+				initialRoute={{title: 'My Initial Scene', index: 1}}
+				renderScene={(route, navigator) =>
+					<MyScene
+						title={route.title}
+						// Function to call when a new scene should be displayed
+						onForward={ () => {
+							const nextIndex = route.index + 1;
+							navigator.push({
+								title: 'Scene ' + nextIndex,
+								index: nextIndex,
+							});
+						}}
+						// Function to call to go back to the previous scene
+						onBack={() => {
+							if (route.index > 0) {
+								navigator.pop();
+							}
+						}}
+					/>
+				}
+			/>
+		)
 	}
 }
 
-const styles = StyleSheet.create({
-	red: {
-		color: "red"
-	},
-	bigblue: {
-		color: "blue",
-		fontWeight: "bold",
-		fontSize: 30,
-	},
-});
-
-AppRegistry.registerComponent('ReactNative_intelliSchool', () => ReactNative_intelliSchool);
+class MyScene extends Component {
+	render() {
+		return (
+			<View style={{paddingTop: 50}}>
+				<Text>Current Scene: { this.props.title }</Text>
+				<TouchableHighlight onPress={this.props.onForward}>
+					<Text>Tap me to load the next scene</Text>
+				</TouchableHighlight>
+				<TouchableHighlight onPress={this.props.onBack}>
+					<Text>Tap me to go back</Text>
+				</TouchableHighlight>
+			</View>
+		)
+	}
+}
+AppRegistry.registerComponent('ReactNative_intelliSchool', () => SimpleNavigationApp);
