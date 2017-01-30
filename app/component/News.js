f47b3dc38755560ef17a7ef17f579579f8301f82
JohnTrump <wujunchuan1994@hotmail.com>
2017-01-30 16:24:54 +0800

    NewsItemWithPicture 静态样式OK

--- app/component/News.js
+++ app/component/News.js
@@ -15,7 +15,7 @@ import {
 import NewsDetail from './NewsDetail';
 import ScrollableTabView, {DefaultTabBar} from 'react-native-scrollable-tab-view';
 import NewsList from './NewsList';
-
+import NewsItemWithPicture from './NewsItemWithPicture'
 export default class News extends Component {
     constructor(props) {
         super(props);
@@ -31,18 +31,13 @@ export default class News extends Component {
                 tabBarUnderlineStyle={{backgroundColor: '#FFFFFF', height: 3}}
                 renderTabBar={() => <DefaultTabBar tabStyle={{paddingBottom: 0}}/>}
             >
-                <NewsList style={styles.tabContent} tabLabel="校园新闻"/>
-                <View style={styles.tabContent} tabLabel="通知公告">
-                    <TouchableOpacity onPress={() => {
-                        this.props.navigator.push({
-                            component: NewsDetail,
-                            title: "新闻详情",
-                        })
-                    }}>
-                        <View style={styles.button}><Text style={styles.buttonText}>很多新闻</Text></View>
-                    </TouchableOpacity>
+                <NewsList tabLabel="校园新闻"/>
+                <View tabLabel="通知公告">
+                    <NewsItemWithPicture/>
+                    <NewsItemWithPicture/>
+                    <NewsItemWithPicture/>
                 </View>
-                <View style={styles.tabContent} tabLabel="党建工作">
+                <View tabLabel="党建工作">
                     <TouchableOpacity onPress={() => {
                         this.props.navigator.push({
                             component: NewsDetail,
@@ -66,10 +61,5 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#F5FCFF',
-    },
-    tabContent: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
     },
 });
--- app/component/NewsItemWithPicture.js
+++ app/component/NewsItemWithPicture.js
@@ -1,4 +1,81 @@
 /**
+ * 列表项--有图片
  * @author junchuan.wu
  * @date :  2017/1/30
  */
+import React, {Component} from 'react'
+import {StyleSheet, View, Text,Image} from 'react-native'
+export default class NewsItemWithPicture extends Component {
+    static defaultProps = {};
+    static propTypes = {};
+
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            /*容器*/
+            <View style={styles.container}>
+                <View style={styles.containerLeft}>
+                    <Text numberOfLines={2} style={styles.title}>
+                        校企共建"帝启互联网金融学院"签约及揭牌仪式顺利召开
+                        校企共建"帝启互联网金融学院"签约及揭牌仪式顺利召开
+                    </Text>
+                    <View style={styles.info}>
+                        <Text style={styles.infoFont}>阅读:15318</Text>
+                        <Text style={styles.infoFont}>创新创业部</Text>
+                        <Text style={styles.infoFont}>2016-12-14</Text>
+                    </View>
+                </View>
+                <View style={styles.containerRight}>
+                    <Image style={styles.img} source={{uri:'http://se.xmut.edu.cn/file/2016/12/14/14816866788163T17T.png'}}/>
+                </View>
+            </View>
+        );
+    }
+}
+const styles = StyleSheet.create({
+    container:{
+        flexDirection:'row',
+        backgroundColor:'white',
+        marginBottom:8,
+        height:75,
+        shadowColor: 'rgba(0,0,0,50)',
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowRadius: 4,
+        shadowOpacity: 0.4,
+    },
+    /*左边*/
+    containerLeft:{
+        justifyContent:'space-between',
+        marginTop:8,
+        marginBottom:8,
+        marginLeft: 13,
+        marginRight:8,
+        width:255
+    },
+    /*右边,放照片*/
+    containerRight:{},
+    img:{
+        width:90,
+        height:60,
+        marginTop:8,
+        marginRight:13
+    },
+    info:{
+        flexDirection:'row',
+        justifyContent:'space-between',
+    },
+    infoFont:{
+        color:'#dddedf',
+        fontSize:11
+    },
+    title:{
+        fontSize:16
+    }
+});
