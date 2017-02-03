51f4893057c55d5eac7bd35ca42265308c7734e3
JohnTrump <wujunchuan1994@hotmail.com>
2017-02-03 15:25:38 +0800

    android index

--- index.android.js
+++ index.android.js
@@ -4,50 +4,98 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    StyleSheet,
+    View,
+    Text,
+    StatusBar,
+    AsyncStorage
 } from 'react-native';
-
+import {Button, Card} from 'react-native-material-design';
+import GuideNavigator from './app/component/GuideNavigator'
+import Entrance from './app/component/Entrance';
+import Util from './app/Utils';
 export default class ReactNative_intelliSchool extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            /*是否显示启动页面,Notice:远程调试状态下可能会卡顿!!*/
+            showStart: true,
+            /*是否显示初次启动页面的导航页,如果是第一次打开的话,就是true,否则为false*/
+            showGuide: true
+        };
+    }
+
+    /**
+     * 在组件在渲染先前获取是否第一次打开app
+     */
+    componentWillMount() {
+        this._getFirst();
+    }
+
+    /**
+     * 从AsyncStorage中取是否为第一次登陆的标记
+     * @private
+     */
+    _getFirst() {
+        AsyncStorage.getItem("isfirst")
+            .then((value) => {
+                if (value !== null) {
+                    this.setState({
+                        /*因为AsyncStorage取出来的都是String类型,我这里用JSON库直接进行转换TODO:方便测试,下面这行注释*/
+                        /*showGuide: JSON.parse(value)*/
+                    });
+                } else {
+                    console.warn("没有isfirst记录");
+                }
+            })
+            .catch((error) => {
+                console.error("读取isfirst发生错误..");
+                console.log(error);
+            }).done();
+    }
+
+    _hideEntrance() {
+        this.setState({
+            showStart: false
+        })
+    }
+
+    render() {
+        /*判断是否是第一次打开APP应用*/
+        let entrance = null;
+        if (this.state.showGuide) {
+            if (this.state.showStart) {
+                entrance = <Entrance hideThis={() => this._hideEntrance()}/>
+            } else {
+                entrance = <View style={styles.secondView}><GuideNavigator/></View>
+            }
+        } else {
+            if (this.state.showStart) {
+                entrance = <Entrance hideThis={() => this._hideEntrance()}/>
+            } else {
+                entrance = <View style={styles.secondView}><Text>Hello Android</Text></View>
+            }
+        }
+
+        return (
+            <View style={{width: Util.getScreenParam().size.width, height: Util.getScreenParam().size.height}}>
+                {entrance}
+            </View>
+        );
+    }
 }
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    secondView: {
+        position: 'absolute',
+        width: Util.getScreenParam().size.width,
+        height: Util.getScreenParam().size.height,
+        backgroundColor: 'green'
+    }
 });
 
 AppRegistry.registerComponent('ReactNative_intelliSchool', () => ReactNative_intelliSchool);
