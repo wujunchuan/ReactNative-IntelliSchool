5119fd61eb15303e13040160b9d8c7c774fa5664
JohnTrump <wujunchuan1994@hotmail.com>
2017-02-28 23:46:47 +0800

    获取天气数据

---
+++ app/component/Ecard.js
@@ -0,0 +1,25 @@
+/**
+ * E通卡查询
+ * @author junchuan.wu
+ * @date :  2017/2/28
+ */
+import React, {Component} from 'react'
+import {StyleSheet, View, Text} from 'react-native'
+export default class Ecard extends Component {
+    static defaultProps = {};
+    static propTypes = {};
+
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <View>
+                <Text>Hello world</Text>
+            </View>
+        );
+    }
+};
+const styles = StyleSheet.create({});
--- app/component/Weather.js
+++ app/component/Weather.js
@@ -12,38 +12,85 @@ import {
     ScrollView,
     TouchableOpacity,
 } from 'react-native';
-
+import Utils from '../Utils';
+import Spinner from 'react-native-loading-spinner-overlay';
 export default class NewsDetail extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            isLoadding:true,
+            weather:{
+                future:{
+                    'day_20170228':{
+                        weather_id:{
+                            fa:'0',
+                            fb:'0'
+                        }
+                    }
+                },//未来天气
+                sk:{},//当前天气
+                today:{
+                    weather_id:{
+                        fa:'0',
+                        fb:'0',
+                    }
+                },//今日天气
+            }
+        };
         //this._testJSON();
+        this._getWeatherJSON('厦门');
     }
 
     render() {
+        let loading = this.state.isLoading;
+        let today = this.state.weather.today;
+        let sk = this.state.weather.sk;
+        let future = this.state.weather.future;
         return (
             <View style={styles.container}>
-
+                <Spinner visible={loading}/>
+                <Text>时间:{today.date_y} {today.week}  {sk.time} </Text>
+                <Text>地点:{today.city} </Text>
+                <Text>建议:{today.dressing_advice}</Text>
+                <Text>温度区间:{today.temperature}</Text>
+                <Text>当前温度:{sk.temp}℃</Text>
+                <Text>1天气符号:{today.weather_id.fa}</Text>
+                <Text>2天气符号:{today.weather_id.fb}</Text>
+                <Text>power by 中国气象局</Text>
+                {Object.keys(future).map((name, index)=>{
+                    let day = this.state.weather.future[name];
+                    return (
+                        <View key={index}>
+                            <Text>temperature:{day.temperature}</Text>
+                            <Text>weather:{day.weather}</Text>
+                            <Text>weather图标1:{day.weather_id.fa}</Text>
+                            <Text>weather图标2:{day.weather_id.fb}</Text>
+                            <Text>week:{day.week}</Text>
+                        </View>
+                    )
+                })}
             </View>
         );
     }
-    _testJSON(){
+
+    _testJSON() {
         /*uri:http://7xvzr6.com1.z0.glb.clouddn.com/weather.json*/
-        fetch('http://7xvzr6.com1.z0.glb.clouddn.com/weather.json').then((value)=>{
+        fetch('http://7xvzr6.com1.z0.glb.clouddn.com/weather.json').then((value) => {
             console.log(value);
         })
     }
-    _getWeatherJSON() {
-        const params = {
-            cityname: '厦门', /*城市名称由定位来*/
-            dtype: 'json',
-            key: '55747d2964437629d3f6db58f84ee44b',
-            format: 1
-        };
-        const _weatherApi = encodeURI(`http://v.juhe.cn/weather/index?cityname=${params.cityname}&dtype=${params.dtype}&key=${params.key}`);
-        fetch(_weatherApi).then((value) => {
-            console.log(value);
-        }).catch((error) => {
-            console.log(error);
+
+    _getWeatherJSON(cityname) {
+        Utils.get(`/weather?cityname=${cityname}`, (data) => {
+            //没有错误
+                this.setState({
+                    isLoading:false,
+                    weather: {
+                        future: data.result.future,
+                        sk: data.result.sk,
+                        today: data.result.today
+                    }
+                });
         });
     }
 
@@ -54,6 +101,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#F5FCFF',
+        marginTop:100
     },
     tabContent: {
         alignItems: 'center',
